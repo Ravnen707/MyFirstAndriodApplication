18fb26767e091b624f6aeed869d1c93cd915c311
ravnen707 <ravnen707@gmail.com>
2021-02-20 13:20:04 +0100

    Milestone 2. Edit text can be shown in textview by a bttnclick

--- app/src/main/java/com/example/myfirstandriodapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstandriodapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myfirstandriodapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            }
+
+    public void bttnOnClick(View view) {
+        TextView tvN = (TextView) findViewById(R.id.txtName);
+        TextView tvNedit = (TextView) findViewById(R.id.editText);
+        tvN.setText(tvNedit.getText());
+        TextView tvA = (TextView) findViewById(R.id.txtAge);
+        TextView tvAedit = (TextView) findViewById(R.id.editText2);
+        tvA.setText(tvAedit.getText());
+
     }
 }
